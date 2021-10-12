fdf82f3aa3de851b200913d19eb9011aff50a43a
Jacob <yawjacob@yahoo.com>
2021-10-12 12:34:35 -0500

    commit tree 3e0e7e10b5f35c659e72236e1167b58bb498b76f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function superbowlWin(array) {
+    const found = array.find(function(object) {
+   return object.result === "W"
+    })
+    if (found) {
+      return found.year
+    }
+    else {
+    return found
+    }
+}
